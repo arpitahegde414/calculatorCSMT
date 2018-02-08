07dc61b27f683d257baa47ebaf5f5903424b7c9e
Arpita Hegde <33028166+arpitahegde414@users.noreply.github.com>
2018-02-08 22:10:15 +0530

    Delete CalculatorInterface.java

--- CalculatorInterface.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-public interface CalculatorInterface {
-	public <T extends Number>double add(T a,T b);
-    public <T extends Number>double sub(T a,T b);
-    public <T extends Number>double mul(T a,T b);
-    public <T extends Number>double div(T a,T b);
-}
